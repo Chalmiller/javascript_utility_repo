825bd5f296f4390a55165431379fcece9e32f902
Chalmiller <chalmiller1@gmail.com>
2020-03-12 09:39:04 -0700

    More time complexity notes

--- big_o_complexity.js
+++ big_o_complexity.js
@@ -37,3 +37,16 @@ function a(n) {
     }
     return count;
 }
+
+//O(5n*n) => O(n^2)
+function a(n) {
+    var count=0;
+    for (var i=0; i<n; i++) {
+        count += 1;
+        for (var i=0; i<5*n; i++) {
+            count += 1;
+        }
+    }
+    return count;
+}
+
